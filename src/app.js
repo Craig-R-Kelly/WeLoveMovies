07c99f233fe724129592657bc6ccecc2ebfd3b5f
Craig R. Kelly <craig-r-kelly@hotmail.com>
2024-03-14 15:56:58 -0500

    more cors

--- src/app.js
+++ src/app.js
@@ -5,7 +5,15 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: 'https://movies-frontend-4epd.onrender.com', 
+  optionsSuccessStatus: 200, 
+  credentials: true, 
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 // added the line above and the block below for deployment
